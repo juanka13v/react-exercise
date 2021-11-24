33bc80eaf7a187acab6c61b861437aabf80da36e
Juan Valero <valerojuan13c@gmail.com>
2021-11-24 03:15:55 -0300

    cocktail

--- src/context.jsx
+++ src/context.jsx
@@ -1,51 +1,65 @@
-import React, { useState, useContext } from "react";
-import sublinks from "./data";
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback } from "react";
 
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 const AppContext = React.createContext();
 
-export const AppProvider = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
-  const [location, setLocation] = useState({});
-  const [page, setPage] = useState({page:'', links:[]});
+const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("a");
+  const [cocktails, setCocktails] = useState([]);
 
-  const openSidebar = () => {
-    setIsSidebarOpen(true);
+  const fetchDrinks = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      const { drinks } = data;
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+
+        setCocktails(newCocktails)
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
   };
 
-  const closeSidebar = () => {
-    setIsSidebarOpen(false);
-  };
-
-  const openSubmenu = (text, coordinates) => {
-    const page = sublinks.find((link) => link.page === text)
-    setPage(page)
-    setLocation(coordinates);
-    setIsSubmenuOpen(true);
-  };
-
-  const closeSubmenu = () => {
-    setIsSubmenuOpen(false);
-  };
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm]);
 
   return (
     <AppContext.Provider
       value={{
-        isSubmenuOpen,
-        openSubmenu,
-        closeSubmenu,
-        isSidebarOpen,
-        openSidebar,
-        closeSidebar,
-        location,
-        page
+        loading,
+        searchTerm,
+        cocktails,
+        setSearchTerm
       }}
     >
       {children}
     </AppContext.Provider>
   );
 };
-
+// make sure use
 export const useGlobalContext = () => {
-    return useContext(AppContext);
-}
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">
+        about us
+      </h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo error, ipsum libero veniam eligendi minus ea accusantium dignissimos mollitia tempore provident amet nulla, consectetur doloremque voluptatum. Voluptatum aut quis doloribus!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut eaque consequuntur veniam ab deserunt nisi officia voluptatibus ratione provident? Neque est obcaecati itaque quibusdam illum sunt vitae ipsum natus magnam.
+      </p>
+    </section>
+  )
+}
+
+export default About
